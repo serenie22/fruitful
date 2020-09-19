198bf1f5cb88cc42929aa05267fb9e62f18b6d04
Divya Aggarwal <divyaaggarwal@Divyas-MacBook-Pro.local>
2020-09-19 08:47:57 -0700

    main page and scrolling activity

--- app/src/main/java/com/example/fruitful/MainActivity.java
+++ app/src/main/java/com/example/fruitful/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.fruitful;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
     }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        startActivity(intent);
+    }
+
 }
