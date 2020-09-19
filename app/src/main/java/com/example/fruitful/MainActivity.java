988d4e27898410d8fa34397a6837a8bcc11cad79
serenie22 <sjying@andrew.cmu.edu>
2020-09-19 17:52:08 -0400

    partially finished layouts

--- app/src/main/java/com/example/fruitful/MainActivity.java
+++ app/src/main/java/com/example/fruitful/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.fruitful;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login, simpleButton2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.login);//get id of button 1
+
     }
 }
